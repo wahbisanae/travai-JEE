4fffc3c115f49a2c25ee9323df597e6f35bebfc0
wahbisanae <wahbisanae03@gmail.com>
2024-09-30 20:38:29 +0200

    Add files via upload

---
+++ scripts des developpeurs/src/scripts/des/developpeurs/ScriptsDesDeveloppeurs.java
@@ -0,0 +1,32 @@
+
+package scripts.des.developpeurs;
+
+//import static test.Test.createtable;
+
+import static exo.ExoJDBC.executeFreeQuery;
+import static exo.Nombrescript.nombrescript;
+import static exo.Nombretotalescript.nombretotalescript;
+import static exo.Nombrmax.nombrmax;
+import static exo.Ordredec.ordredec;
+import static exo.Ordredec.ordredec;
+import static test.Test.createtable;
+import static test.Test.insertion;
+
+
+
+
+public class ScriptsDesDeveloppeurs{
+    public static void main(String[] args) {
+
+        //createtable();
+        //nombrmax();
+       //ordredec();
+        //nombrescript();
+        //nombretotalescript("ALAMI");
+        executeFreeQuery();
+        //insertion();
+    
+}
+
+   
+}
